705f2395c01d7fa6f8e64b9a2be1afe88f85a5ff
Darien Moore <Darienmoore2002@gmail.com>
2023-02-05 22:39:32 -0600

    created ScoreTypes and UserAccountHistoryTypes enums

--- losuapi/enums.py
+++ losuapi/enums.py
@@ -3,5 +3,15 @@ from enum import Enum
 class GameMode(str, Enum):
     OSU = "osu"
     MANIA = "mania"
-    FRUITS = "fruits"
     TAIKO = "taiko"
+    FRUITS = "fruits"
+
+class ScoreTypes(str, Enum):
+    BEST = "best"
+    FIRSTS = "firsts"
+    RECENT = "recent"
+    
+class UserAcountHistoryTypes(str, Enum):
+    NOTE = "note"
+    RESTRICTION = "restriction"
+    SILENCE = "silence"
