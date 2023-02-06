bb92ea6f5b54beaf4ab34a797ef9bc52b6b8769b
Darien Moore <Darienmoore2002@gmail.com>
2023-02-05 22:39:58 -0600

    completed some models

--- losuapi/models.py
+++ losuapi/models.py
@@ -1,7 +1,7 @@
 from datetime import datetime
 from pydantic import BaseModel, Field
 from typing import Optional
-from .enums import GameMode
+from .enums import GameMode, UserAcountHistoryTypes
 
 class Covers(BaseModel):
     cover: str
@@ -19,7 +19,7 @@ class Covers(BaseModel):
     
 class Availability(BaseModel):
     download_disabled: bool
-    more_information: Optional[str]
+    more_information: Optional[str] = None
     
 class Hype(BaseModel):
     current: int
@@ -30,8 +30,8 @@ class Nominations(BaseModel):
     required: int
 
 class Failtimes(BaseModel):
-    exit: Optional[list[int]]
-    fail: Optional[list[int]]
+    exit: Optional[list[int]] = None
+    fail: Optional[list[int]] = None
 
 class BeatmapsetCompact(BaseModel):
     artist: str
@@ -61,16 +61,16 @@ class Beatmapset(BeatmapsetCompact):
     can_be_hyped: bool
     creator: str
     discussion_locked: bool
-    hype: Optional[Hype]
+    hype: Optional[Hype] = None
     is_scoreable: bool
     last_updated: datetime
-    legacy_thread_url: Optional[str]
+    legacy_thread_url: Optional[str] = None
     nominations: Nominations = Field(alias="nominations_summary")
     ranked: int
-    ranked_date = Optional[datetime]
+    ranked_date = Optional[datetime] = None
     source: str
     storyboard: bool
-    submitted_date: Optional[datetime]
+    submitted_date: Optional[datetime] = None
     tags: str
 
     class Config:
@@ -101,13 +101,13 @@ class Beatmap(BeatmapCompact):
     accuracy: float
     ar: float
     beatmapset_id: int
-    bpm: Optional[float]
+    bpm: Optional[float] = None
     convert: bool
     count_circles: int
     count_sliders: int
     count_spinners: int
     cs: float
-    deleted_at: Optional[datetime]
+    deleted_at: Optional[datetime] = None
     drain: float
     hit_length: int
     is_scoreable: bool
@@ -124,28 +124,189 @@ class Beatmap(BeatmapCompact):
                 datetime: str,
                 }
 
-# TODO make Score model
-class Score(BaseModel):
-    ...
-	
-# TODO make UserGroup model https://osu.ppy.sh/docs/index.html#usergroup
+# https://osu.ppy.sh/docs/index.html#usergroup
 class UserGroup(BaseModel):
-    ...
+    playmodes: Optional[list[GameMode]] = None
     
-# TODO make UserAccountHistory model https://osu.ppy.sh/docs/index.html#usercompact-useraccounthistory
+# https://osu.ppy.sh/docs/index.html#usercompact-useraccounthistory
 class UserAccountHistory(BaseModel):
-    ...
+    description: Optional[str] = None
+    id: int
+    length: int
+    permanent: bool
+    timestamp: datetime
+    history_type: UserAcountHistoryTypes = Field(alias="type")
     
-# TODO make UserBadge model https://osu.ppy.sh/docs/index.html#usercompact-userbadge
+# https://osu.ppy.sh/docs/index.html#usercompact-userbadge
 class UserBadge(BaseModel):
+    awarded_at: datetime
+    description: str
+    image_url: str
+    url: str
+
+# https://osu.ppy.sh/docs/index.html#usercompact-profilebanner
+class ProfileBanner(BaseModel):
+    id: int
+    tournament_id: int
+    image: str
+    
+# https://osu.ppy.sh/docs/index.html#usercompact-rankhighest
+class RankHighest(BaseModel):
+    rank: int
+    updated_at: datetime
+
+# https://osu.ppy.sh/docs/index.html#beatmapdifficultyattributes
+class DifficultyAttributes(BaseModel):
+    max_combo: int
+    star_rating: float
+    aim_difficulty: float
+    approach_rate: float
+    flastlight_difficulty: float
+    overall_difficulty: float
+    slider_factor: float
+    speed_difficulty: float
+    stamina_difficulty: float
+    rhythm_difficulty: float
+    color_difficulty: float = Field(alias="colour_difficulty")
+    great_hit_window: float
+    score_multiplayer: float
+
+    class Config:
+        allow_population_by_field_name = True
+
+class Attributes(BaseModel):
+    attributes: DifficultyAttributes
+
+    class Config:
+        arbitrary_types_allowed = True
+
+# https://osu.ppy.sh/docs/index.html#kudosuhistory
+class Giver(BaseModel):
+    url: str
+    username: str
+
+# https://osu.ppy.sh/docs/index.html#kudosuhistory
+class Post:
+    url: Optional[str] = None
+    title: str
+
+# https://osu.ppy.sh/docs/index.html#kudosuhistory
+class KudosuHistory(BaseModel):
+    id: int
+    action: str
+    account: int
+    model: str
+    created_at: datetime
+    giver: Optional[Giver] = None
+    post: Post
+
+    class Config:
+        allow_population_by_field_name = True
+        arbitrary_types_allowed = True
+        json_encoders = {
+                datetime: str,
+                }
+
+class Weight(BaseModel):
+    percentage: float
+    pp: int
+
+class UserMonthlyPlaycount(BaseModel):
+    start_date: datetime
+    count: int
+
+    class Config:
+        arbitrary_types_allowed = True
+        json_encoders = {
+            datetime: str,
+        }
+
+class Cover(BaseModel):
+    custom_url: str
+    url: str
+    id: int
+
+class Country(BaseModel):
+    code: str
+    name: str
+
+class Page(BaseModel):
+    html: str
+    raw: str
+
+class RankHistory(BaseModel):
+    mode: str
+    data: list[int]
+
+class ReplaysWatchedCount(BaseModel):
+    start_date: str
+    count: int
+
+# TODO make UserStatistics model https://osu.ppy.sh/docs/index.html#userstatistics
+class UserStatistics(BaseModel):
     ...
 
-# TODO make ProfileBanner model https://osu.ppy.sh/docs/index.html#usercompact-profilebanner
-class ProfileBanner(BaseModel):
-    ...
-    
-# TODO make RankHighest model https://osu.ppy.sh/docs/index.html#usercompact-rankhighest
-class RankHighest(BaseModel):
+class UserStatistics(BaseModel):
+    osu: Optional[UserStatistics] = None
+    mania: Optional[UserStatistics] = None
+    taiko: Optional[UserStatistics] = None
+    fruits: Optional[UserStatistics] = None
+
+    class Config:
+        arbitrary_types_allowed = True
+
+# https://osu.ppy.sh/docs/index.html#usercompact
+class UserCompact(BaseModel):
+    avatar_url: str
+    country_code: str
+    default_group: str
+    id: int
+    is_active: bool
+    is_bot: bool
+    is_deleted: bool
+    is_online: bool
+    is_supporter: bool
+    last_vist: Optional[datetime] = None
+    pm_friends_only: bool
+    profile_color: Optional[str] = Field(alias="profile_colour", default=None)
+    username: str
+
+    # https://osu.ppy.sh/docs/index.html#usercompact-optionalattributes
+    account_history: list[UserAccountHistory]
+    active_tournament_banner: Optional[ProfileBanner] = None
+    badges: list[UserBadge]
+    beatmap_playcounts_count: int
+    blocks: any
+    country: Country
+    cover: Cover
+    favorite_beatmapset_count: int = Field(alias="favorite_beatmapset_count")
+    follower_count: int
+    graveyard_beatmapset_count: int
+    groups: list[UserGroup]
+    is_restricted: bool
+    loved_beatmapset_count: int
+    monthly_playcounts: list[UserMonthlyPlaycount]
+    page: Page
+    pending_beatmapset_count: int
+    previous_usernames: list[str]
+    rank_highest: Optional[RankHighest] = None
+    rank_history: RankHistory
+    ranked_beatmapset_count: int
+    replays_watched_count: list[ReplaysWatchedCount]
+    score__best_count: int
+    score_first_count: int
+    scores_recent_count: int
+    statistics: UserStatistics
+
+    class Config:
+        allow_population_by_field_name = True
+        arbitrary_types_allowed = True
+        json_encoders = {
+                datetime: str,
+                }
+
+# TODO make Score model
+class Score(BaseModel):
     ...
     
 # https://osu.ppy.sh/docs/index.html#beatmapuserscore
@@ -156,7 +317,7 @@ class BeatmapUserScore(BaseModel):
 # https://osu.ppy.sh/docs/index.html#beatmapscores
 class BeatmapScores(BaseModel):
     scores: list[Score]
-    userScore: Optional[BeatmapUserScore]
-    
+    userScore: Optional[BeatmapUserScore] = None
 
-    
+    class Config:
+        arbitrary_types_allowed = True
