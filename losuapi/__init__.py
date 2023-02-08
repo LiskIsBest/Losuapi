500e9ddf8bea7ddca31984f1d9d7fa1f338f9158
Darien Moore <89608814+LiskIsBest@users.noreply.github.com>
2023-02-08 09:26:21 +0000

    updated imports

--- losuapi/__init__.py
+++ losuapi/__init__.py
@@ -1,13 +1,2 @@
 from .api import OsuApi
-from .types.Beatmap import Beatmap, BeatmapCompact
-from .types.Beatmapset import Beatmapset, BeatmapsetCompact
-from .types.Covers import Covers
-from .types.DifficultyAttributes import DifficultyAttributes
-from .types.enums import UserAcountHistoryTypes, UserListFilters, UserListSorts, UserListViews, GameMode, GameModeInt, ScoreTypes, RankingType, BeatmapsetDownload
-from .types.Extras import GradeCounts, Level, Cover, Country, RankHighest, RankHistory, ReplaysWatchedCount, ProfileBanner, Availability, Hype, Nominations, Weight, Page, Kudosu, Statistics, ScoreMatchInfo, Attributes, Giver, Post, Failtimes
-from .types.KudosuHistory import KudosuHistory
-from .types.Rankings import Spotlight, Spotlights, Cursor, Rankings
-from .types.Score import Score, BeatmapScores, BeatmapUserScore, Scores
-from .types.User import User, UserCompact
-from .types.UserExtras import UserGroup, UserAccountHistory, UserBadge, UserMonthlyPlaycount, UserAchievement, UserProfileCustomization
-from .types.UserStatistics import UserStatistics, UserStatisticsRulesets
+import types
