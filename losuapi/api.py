0b7fdb1b19790c865f3915847889f9abb6062cbb
Darien Moore <Darienmoore2002@gmail.com>
2023-02-09 03:43:49 -0600

    Scores endpoint working, error handling finished for existing endpoints

--- losuapi/api.py
+++ losuapi/api.py
@@ -1,5 +1,5 @@
 import httpx
-from .types import Beatmap, Rankings, User, Score, GameMode, RankingType, ScoreTypes
+from .types import Beatmap, Rankings, User, Score, Scores,GameMode, RankingType, ScoreTypes
 from pydantic import parse_obj_as
 
 BASE_URL = "https://osu.ppy.sh/api/v2"
@@ -26,34 +26,39 @@ class OsuApi:
         }
 
         response = httpx.post(url=TOKEN_URL, json=body_params, headers=self.base_headers).json()
+        if "error" in response:
+            raise ConnectionError(f'error: {response["error"]}')
         return response["token_type"], response["access_token"]
 
     # * https://osu.ppy.sh/docs/index.html#lookup-beatmap
-    def lookup_beatmap(self, beatmap_id:int|str, checksum:str="", filename:str="") -> Beatmap:
-        if type(beatmap_id) == int:
-            beatmap_id = str(beatmap_id)
+    def lookup_beatmap(self, 
+                       beatmap_id:int|str, 
+                       checksum:str="", 
+                       filename:str="") -> Beatmap:
 
         headers = self.base_headers
         headers["Authorization"] = self.authorization
 
-        query_params = {
-            "checksum" : checksum,
-            "filename" : filename,
-            "id" : beatmap_id,
-        }
+        query_params = {}
 
         if not isinstance(beatmap_id, (int,str)):
-            raise ValueError("username must be an int or str")
+            raise ValueError("param:username must be type<int> or type<str>")
         else:
             query_params["id"] = beatmap_id
 
         if checksum != "":
             if isinstance(checksum, str):
-                raise TypeError()
+                raise TypeError("param:checksum must be type<str>")
+            query_params["checksum"] = checksum
+
+        if filename != "":
+            if isinstance(filename, str):
+                raise TypeError("param:filename must be type<str>")
+            query_params["filename"] = filename
 
 
-        response = httpx.get(url=BASE_URL+"/beatmaps/lookup", params=query_params, headers=headers)
-        return parse_obj_as(type_=Beatmap, obj=response.json())
+        response = httpx.get(url=BASE_URL+"/beatmaps/lookup", params=query_params, headers=headers).json()
+        return parse_obj_as(type_=Beatmap, obj=response)
     
     # TODO make user_beatmap_score request https://osu.ppy.sh/docs/index.html#get-a-user-beatmap-score
     def user_beatmap_score(self):
@@ -97,29 +102,30 @@ class OsuApi:
         query_params = {}
         
         if include_fails != 0:
-            if type != ScoreTypes.RECENT:
+            if type != ScoreTypes.RECENT.value:
                 raise ValueError("type must be 'recent' in order to set include_fails")
             elif type != 1:
-                raise ValueError("include_fails must be either 1 or 0")
+                raise ValueError("include_fails can only be either 1 or 0")
             query_params["include_fails"] = include_fails
 
         if mode != None: 
-            if mode in GameMode.__members__.values():
+            if mode in GameMode.list():
                 query_params["mode"] = mode
             else:
-                raise ValueError("mode not in ['osu','taiko','fruits','mania']")
+                raise ValueError("mode can only be 'osu','taiko','fruits', or 'mania'")
 
-        if limit != None and isinstance(limit, int):
+        if limit != None:
+            if isinstance(limit, int):
+                raise TypeError("param:limit must be type<int>")    
             query_params["limit"] = limit
             
-        if offset!= None and isinstance(offset, int):
+        if offset!= None:
+            if isinstance(offset, int):
+                raise TypeError("param:offset must be type<int>")
             query_params["offset"] = offset
 
         response = httpx.get(url=BASE_URL+f"/users/{user_id}/scores/{type}", headers=self.base_headers, params=query_params).json()
-        Scores = []
-        for score in response:
-            Scores.append(parse_obj_as(type_=Score, obj=score))
-        return Scores
+        return parse_obj_as(type_=Scores, obj=response)
                 
     # TODO make user_beatmaps request https://osu.ppy.sh/docs/index.html#get-user-beatmaps
     def user_beatmaps(self):
@@ -131,9 +137,9 @@ class OsuApi:
     
     # * https://osu.ppy.sh/docs/index.html#get-user
     def user(self,
-            username:int|str,
-            mode:str="",
-            key:str=None):
+             username:int|str,
+             mode:str="",
+             key:str=None):
         
         headers = self.base_headers
         headers["Authorization"] = self.authorization
@@ -141,20 +147,20 @@ class OsuApi:
         query_params = {}
 
         if not isinstance(username, (int,str)):
-            raise ValueError("username must be an int or str")
+            raise ValueError("param:username must be type<int> or type<str>")
 
         if mode != "": 
-            if mode not in GameMode.__members__.values():
+            if mode not in GameMode.list():
                 raise ValueError("mode not in ['osu','taiko','fruits','mania']")
             query_params["mode"] = mode
 
         if key != None:
             if key not in ["id", "username"]:
-                raise ValueError("key must be in ['id','username']")
+                raise ValueError("key can only be 'id' or 'username'")
             query_params["key"] = key
 
-        response = httpx.get(url=BASE_URL+f"/users/{username}/{mode}", headers=headers,params=query_params)
-        return parse_obj_as(type_=User, obj=response.json())
+        response = httpx.get(url=BASE_URL+f"/users/{username}/{mode}", headers=headers,params=query_params).json()
+        return parse_obj_as(type_=User, obj=response)
 
     # TODO make users request https://osu.ppy.sh/docs/index.html#get-users
     def users(self):
@@ -189,12 +195,12 @@ class OsuApi:
         if country != None:
             if not isinstance(country, int):
                 raise TypeError("country must be an int")
-            if type != RankingType.PERFORMANCE:
+            if type != RankingType.PERFORMANCE.value:
                 raise ValueError("type must be 'performance' in order to set a country code.")
             query_params["country"] = country
 
         if spotlight_id != None:
-            if type != RankingType.CHARTS:
+            if type != RankingType.CHARTS.value:
                 raise ValueError("type must be 'charts' in order to set a spotlight_id.")
             query_params["spotlight"] = spotlight_id
 
@@ -203,14 +209,14 @@ class OsuApi:
         elif variant not in ["4k","7k"]:
             raise ValueError("variant can only be '4k' or '7k'")
         else:
-            if type != RankingType.PERFORMANCE:
+            if type != RankingType.PERFORMANCE.value:
                 raise ValueError("type must be 'performance'")
-            if mode != GameMode.MANIA:
+            if mode != GameMode.MANIA.value:
                 raise ValueError("mode must be 'mania'")
             query_params["variant"] = variant
 
-        response = httpx.get(url=BASE_URL+f"/rankings/{mode}/{type}", headers=headers, params=query_params)
-        return parse_obj_as(type_=Rankings, obj=response.json())
+        response = httpx.get(url=BASE_URL+f"/rankings/{mode}/{type}", headers=headers, params=query_params).json()
+        return parse_obj_as(type_=Rankings, obj=response)
     
     # TODO make spotlights request https://osu.ppy.sh/docs/index.html#get-spotlights
     def spotlights(self):
