fad647b4afd39d1d6b6af9a73f3a5da3c5a0510b
Darien Moore <Darienmoore2002@gmail.com>
2023-02-09 08:05:40 -0600

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages, setup
 with open("README.md", "r") as readme:
     long_description = readme.read()
 
-VERSION = "0.0.3"
+VERSION = "0.0.4"
 
 setup(
     name="losuapi",
@@ -19,7 +19,7 @@ setup(
     license="GPLv3",
     packages=find_packages(),
     install_requires=[
-		"requests",
+		"httpx",
         "pydantic"
 	]
 )
