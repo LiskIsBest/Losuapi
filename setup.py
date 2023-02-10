856e726289fb706aff354a84a9981aba8e022540
Darien Moore <Darienmoore2002@gmail.com>
2023-02-09 20:18:21 -0600

    updated version 0.0.4 -> 0.0.5

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages, setup
 with open("README.md", "r") as readme:
     long_description = readme.read()
 
-VERSION = "0.0.4"
+VERSION = "0.0.5"
 
 setup(
     name="losuapi",
