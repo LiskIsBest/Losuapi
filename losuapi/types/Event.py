40571f839a024108cf627b4e2700a2083871b181
LiskIsBest <darienmoore2002@gmail.com>
2023-02-12 03:57:38 -0600

    created Event model

---
+++ losuapi/types/Event.py
@@ -0,0 +1,38 @@
+from datetime import datetime	
+from pydantic import BaseModel
+from .Extras import Achievement
+from .Enums import GameMode
+
+class EventBeatmap(BaseModel):
+	title: str
+	url: str
+
+class EventBeatmapset(BaseModel):
+	title: str
+	url: str
+
+class EventUser(BaseModel):
+	username: str
+	url: str
+	previousUsername: str | None
+
+class Event(BaseModel):
+	created_at: datetime
+	id: int
+	type: str
+	achievement: Achievement | None
+	user: EventUser | None
+	beatmap: EventBeatmap | None
+	count: int | None
+	approval: str | None
+	beatmapset: EventBeatmapset | None
+	scoreRank: str | None
+	rank: int | None
+	mode: GameMode | None
+
+	class Config:
+		arbitrary_types_allowed = True
+		use_enum_values = True
+		json_encoders = {
+			datetime: str
+		}
