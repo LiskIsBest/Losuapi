71f416b364ef202b6d22cfd9420cca6d7774e313
Darien Moore <Darienmoore2002@gmail.com>
2023-02-12 06:27:53 -0600

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages, setup
 with open("README.md", "r") as readme:
     long_description = readme.read()
 
-VERSION = "0.0.7"
+VERSION = "0.0.8"
 
 setup(
     name="losuapi",
